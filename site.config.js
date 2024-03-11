3b2f9a1f0f67ba2ddda811fb0f623bff26851ab7
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-03-12 01:22:11 +0900

    3/12

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "m1nddoong",
+    image: "/profile_img.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Java Backend Dev",
+    bio: "공부한것들 차곡차곡 정리하기",
+    email: "alstjsdlr990321@gmail.com",
+    linkedin: "m1nddoong990321",
+    github: "m1nddoong",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `velog`,
+      href: "https://velog.io/@alstjsdlr0321",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "m1nddoong-log",
+    description: "welcome to m1nddoong-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://m1nddong-log.vercel.app/",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -35,21 +35,15 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: false,
+    enable: true,
     config: {
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
-    },
-  },
-  naverSearchAdvisor: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
     },
   },
   utterances: {
--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -5,22 +5,27 @@ import styled from "@emotion/styled"
 import { zIndexes } from "src/styles/zIndexes"
 
 type Props = {
-  fullWidth: boolean
+  fullWidth?: boolean
+  readingProgress: number
 }
 
-const Header: React.FC<Props> = ({ fullWidth }) => {
+const Header: React.FC<Props> = ({ fullWidth, readingProgress }) => {
   return (
-    <StyledWrapper>
-      <div data-full-width={fullWidth} className="container">
-        <Logo />
-        <div className="nav">
-          <ThemeToggle />
-          <NavBar />
-        </div>
-      </div>
-    </StyledWrapper>
+      <StyledWrapper>
+        <HeaderWrapper>
+          <div data-full-width={fullWidth} className="container">
+            <Logo />
+            <div className="nav">
+              <ThemeToggle />
+              <NavBar />
+            </div>
+          </div>
+        </HeaderWrapper>
+        <ReadingProgress readingProgress={readingProgress} />
+      </StyledWrapper>
   )
 }
+
 
 export default Header
 
@@ -53,4 +58,40 @@ const StyledWrapper = styled.div`
       align-items: center;
     }
   }
+`
+
+const HeaderWrapper = styled.div`
+  padding: 10px 0;
+  background-color: ${({ theme }) => theme.colors.grayA1};
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  .container {
+    display: flex;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1120px;
+    height: 3rem;
+    margin: 0 auto;
+    &[data-full-width="true"] {
+      @media (min-width: 768px) {
+        padding-left: 6rem;
+        padding-right: 6rem;
+      }
+    }
+    .nav {
+      display: flex;
+      gap: 0.75rem;
+      align-items: center;
+    }
+  }
+`
+
+const ReadingProgress = styled.div<Props>`
+  width: ${({ readingProgress }) => readingProgress + "%"};
+  height: 0.2rem;
+  background-color: ${({ theme }) => theme.colors.amber11};
+  opacity: 0.6;
+  transition: width 0.4s ease-out;
 `
